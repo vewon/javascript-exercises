269e61381b4691c58dad382be7713ddc76b94c4e
Vewon <137260636+vewon@users.noreply.github.com>
2026-01-17 19:06:59 +0800

    Complete 09, 10, and 11 foundations exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0;
+    let startNum = 0;
+    let endNum = 0;
 
+    if ( a > b ) {
+        startNum = b;
+        endNum = a;
+    } else {
+        startNum = a;
+        endNum = b;
+    }
+
+    if ( a < 0 || b < 0 ) {
+        return "ERROR";
+    } else if ( a == "NaN" || b == "NaN" ) {
+        return "ERROR";
+    } else if ( a !== parseInt(a, 10) || b !== parseInt(b , 10) ) {
+        return "ERROR";
+    }
+
+    for ( let i = startNum; i <= endNum; i++ ) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
